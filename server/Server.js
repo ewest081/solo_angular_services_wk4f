467ee6ad17e94d32c82fb9990138a73389cc4d34
Liz Westberg <ewest081@gmail.com>
2016-01-16 12:13:46 -0600

    Remove unnecessary database set up that never got used

--- server/Server.js
+++ server/Server.js
@@ -4,22 +4,9 @@
 var express = require('express');
 var index = require('./routes/index');
 
-//var mongoose = require('mongoose');
-
-//var mongoURI = 'mongodb://localhost:27017/angular_services';
-//var mongoDB = mongoose.connect(mongoURI).connection;
-
 var app = express();
 
 app.use(express.static('server/public'));
-
-//mongoDB.on('error', function(err){
-//    console.log(err);
-//});
-//
-//mongoDB.once('open', function(){
-//    console.log('Connected to MongoDB');
-//});
 
 app.use('/', index);
 
